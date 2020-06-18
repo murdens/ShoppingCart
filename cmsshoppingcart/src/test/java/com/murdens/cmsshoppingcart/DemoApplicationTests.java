07f02180dfa0c2b0cf6b29a5ce98653c95910b62
murdens <sonya.murden@gmail.com>
2020-06-18 23:22:33 +0100

    initial commit springboot project

---
+++ cmsshoppingcart/src/test/java/com/murdens/cmsshoppingcart/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murdens.cmsshoppingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
